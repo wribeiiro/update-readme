5eb4b679590fc9933713cf45aab3b4c776be5fea
Wellisson Ribeiro <welleh10@gmail.com>
2022-07-28 13:45:23 -0300

    :rocket: vrumm

--- index.js
+++ index.js
@@ -2,62 +2,54 @@ const fs = require("fs");
 require("dotenv").config();
 
 const octokit = require("@octokit/core");
+const client = new octokit.Octokit({ auth: process.env.GH_ACCESS_TOKEN });
 
-const client = new octokit.Octokit({ auth: process.env.GITHUB_ACCESS_TOKEN });
-
-async function updateAllRepos() {
+async function updateMyRepository(repositoryName) {
 	try {
-		const response = await client.request("GET /repos/wribeiiro/books", {
-			sort: "updated",
-			per_page: "100",
-		});
-		const repos = response.data.filter(r => r.name !== "geraldiner" && r.name !== "projects-readme" && r.name !== "projects-readme-tutorial");
+		const response = await client.request(`GET /repos/wribeiiro/${repositoryName}`);
+        updateReadMe(response.data.name);
 
-        for (let i = 0; i < repos.length; i++) {
-			const { name } = repos[i];
-			updateReadMe(name);
-		}
 	} catch (error) {
 		console.log(error);
 	}
 }
 
-async function updateReadMe(repo) {
+async function updateReadMe(respository) {
 	try {
-		const res = await client.request(`GET /repos/geraldiner/${repo}/contents/README.md`);
-		const { path, sha, content, encoding } = res.data;
+		const response = await client.request(`GET /repos/wribeiiro/${respository}/contents/README.md`);
+		const { path, sha, content, encoding } = response.data;
 		const rawContent = Buffer.from(content, encoding).toString();
 		const startIndex = rawContent.indexOf("## Other Projects");
-		const updatedContent = `${startIndex === -1 ? rawContent : rawContent.slice(0, startIndex)}\n${getNewProjectSection()}`;
-		commitNewReadme(repo, path, sha, encoding, updatedContent);
+		const updatedContent = `${startIndex === -1 ? rawContent : rawContent.slice(0, startIndex)} \n My happy string \n`;
+
+        commitNewReadme(respository, path, sha, encoding, updatedContent);
 	} catch (error) {
-		try {
-			const content = `\n${getNewProjectSection()}`;
-			await client.request(`PUT /repos/geraldiner/${repo}/contents/README.md`, {
-				message: "Create README",
-				content: Buffer.from(content, "utf-8").toString(encoding),
-			});
-		} catch (err) {
-			console.log(err);
-		}
+		console.log(error)
 	}
 }
 
-async function commitNewReadme(repo, path, sha, encoding, updatedContent) {
+async function commitNewReadme(respository, path, sha, encoding, updatedContent) {
 	try {
-		await client.request(`PUT /repos/geraldiner/${repo}/contents/{path}`, {
-			message: "Update README",
+		const response = await client.request(`PUT /repos/wribeiiro/${respository}/contents/${path}`, {
+			message: "Update README from script",
 			content: Buffer.from(updatedContent, "utf-8").toString(encoding),
 			path,
 			sha,
 		});
-	} catch (err) {
-		console.log(err);
+
+        if (response.status === 200) {
+            console.log('Commit deu deyblade ♟️!!');
+            return;
+        }
+
+        console.log('Commit deu ruim ❌!!');
+	} catch (error) {
+		console.log(error);
 	}
 }
 
-function getNewProjectSection() {
+function fetchNewContent() {
 	return fs.readFileSync("projects.md").toString();
 }
 
-updateAllRepos();
+updateMyRepository('books');
--- projects.md
+++
@@ -1,6 +0,0 @@
-## Other Projects
-
-Check out other stuff I've worked on:
-
-**My personal page**: https://wribeiiro.com
-**My personal page**: https://wribeiiro.com
